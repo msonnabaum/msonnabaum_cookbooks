0f4a978a36f83777e7c20a111e8bd15fe04577f0
Mark Sonnabaum <mark@sonnabaum.com>
2011-03-29 16:53:54 -0500

    Fixed phpunit recipe.

--- phpunit/recipes/default.rb
+++ phpunit/recipes/default.rb
@@ -3,15 +3,11 @@ include_recipe "php"
 channels = %w{pear.phpunit.de components.ez.no pear.symfony-project.com}
 channels.each do |chan|
   php_pear_channel chan do
-    action :discover
+    action [:discover, :update]
   end
 end
 
-# update the main pear channel
-php_pear_channel 'pear.phpunit.net' do
-  action :update
-end
-
-php_pear "phpunit/PHPUnit" do
+php_pear "PHPUnit" do
+  channel "phpunit"
   action :install
 end
